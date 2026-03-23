20a046fdb084cc5dd106f07cd8447d48d76347fc
Rexezuge <79224316+Rexezuge@users.noreply.github.com>
2026-03-23 07:26:47 -0400

    Update eq0.ps1

--- eq0.ps1
+++ eq0.ps1
@@ -12,10 +12,10 @@ Write-Output "Logging in to Azure using Managed Identity..."
 Connect-AzAccount -Identity | Out-Null
 
 $day = (Get-Date).Day
-$mod = $day % 8
+$div = $day / 8
 
 Write-Output "Today day-of-month: $day"
-Write-Output "day % 8 = $mod"
+Write-Output "day / 8 = $div"
 
 $app = Get-AzResource `
     -ResourceGroupName $ResourceGroupName `
@@ -26,7 +26,7 @@ if (-not $app) {
     throw "Container App not found"
 }
 
-if ($mod -eq 0) {
+if ($div -eq 0) {
     Write-Output "Condition met → START container app"
 
     Invoke-AzRestMethod `
